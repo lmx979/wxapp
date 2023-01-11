07e5a226b9bf98d175c5d018b129f7f7f72e9949
lmx979 <1055276312@qq.com>
2023-01-12 02:15:03 +0800

    rank function

--- pages/rank/rank.js
+++ pages/rank/rank.js
@@ -1,66 +1,63 @@
 // pages/rank/rank.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    currentCategory: "project", // 当前排行类别名称
+    currentDate: "week", // 当前排序条件名称
+    rankData: [], // 排行数据
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
+  // 排行种类切换
+  categoryChange(e) {
+    this.setData({
+      currentCategory: e.currentTarget.id
+    })
+    // 记录用户的查看种类状态
+    wx.setStorageSync('statusCategory', e.currentTarget.id)
+    this.onLoad()
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
+  // 排行条件切换
+  dateChange(e) {
+    this.setData({
+      currentDate: e.currentTarget.id,
+    })
+    // 记录用户的查看条件状态
+    wx.setStorageSync('statusDate', e.currentTarget.id)
+    this.onLoad()
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+  onLoad() {
+    const that = this
+    // 获取用户之前记录的查看状态
+    let statusCategory = wx.getStorageSync('statusCategory')
+    let statusDate = wx.getStorageSync('statusDate')
+    this.setData({
+      currentCategory: statusCategory,
+      currentDate: statusDate
+    })
+    // 设置请求地址
+    let apiUrl = ''
+    if (statusCategory == 'project') {
+      if (statusDate == 'week') {
+        apiUrl = 'https://www.fastmock.site/mock/3a01e0b34befd7cef6fcbfd3ab97ccf8/wechat/rank/project/week'
+      } else {
+        apiUrl = 'https://www.fastmock.site/mock/3a01e0b34befd7cef6fcbfd3ab97ccf8/wechat/rank/project/month'
+      }
+    } else if (statusCategory == 'path') {
+      if (statusDate == 'week') {
+        apiUrl = 'https://www.fastmock.site/mock/3a01e0b34befd7cef6fcbfd3ab97ccf8/wechat/rank/path/week'
+      } else {
+        apiUrl = 'https://www.fastmock.site/mock/3a01e0b34befd7cef6fcbfd3ab97ccf8/wechat/rank/path/month'
+      }
+    }
+    // 请求排行数据
+    wx.request({
+      url: apiUrl,
+      header: {
+        'content-type': 'application/json'
+      },
+      success(res) {
+        that.setData({
+          rankData: res.data.data
+        })
+      }
+    })
   }
 })
